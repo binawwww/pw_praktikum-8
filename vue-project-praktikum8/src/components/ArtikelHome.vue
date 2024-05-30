dd2be0d69135989416ef5f37f01605319c16fb90
binawwww <sabrinafebriani17@gmail.com>
2024-05-30 17:09:57 +0700

    pertemuan8

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <img src="../assets/dino2.jpg" alt="" style="height: 200px; ">
+        <h1>Ini Adalah Artikel Home</h1>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+        <p>ini adalah isi artikel home</p>
+
+    </div>
+</template>
